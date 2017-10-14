90e8a46756e034871a269d77addba29a3a70408c
Xu Zhe <xuzhe35@hotmail.com>
2017-10-14 23:30:17 +0800

    极限收益的计算

--- CFO/Combo.cs
+++ CFO/Combo.cs
@@ -11,9 +11,94 @@ namespace CFO
         public List<Position> MemberPos = new List<Position>();
 
         public string Symbol { get; set; }
-        public ComboStrategy Type { get; set; }
+        public ComboStrategy Strategy { get; set; }
         public double FloatingPL { get; set; }
         public Greeks ComboGreeks { get; set; }
+
+        public ProfitType ProfitType { get; set; }
+        public LoseType LoseType { get; set; }
+
+        public double MaxProfit { get; set; }
+        public double MaxLose { get; set; }
+
+        /// <summary>
+        /// 验证一个combo里的所有成员是否都是一个标的物
+        /// </summary>
+        /// <returns></returns>
+        public bool Vaildate()
+        {
+            if (MemberPos.Count > 0)
+            {
+                var symbol = MemberPos[0].Asset.Symbol;
+                //如果成员里但凡有一个不是相同的symbol，则返回验证失败
+                if (MemberPos.Exists((mp) => mp.Asset.Symbol != symbol))
+                    return false;
+                else
+                    return true;
+            }
+            else
+                return false;
+        }
+
+        private void TypeJudge()
+        {
+            if(Strategy == ComboStrategy.BearSpread || Strategy == ComboStrategy.Collar)
+            {
+                ProfitType = ProfitType.LimitedProfit;
+                LoseType = LoseType.LimitedLose;
+            }
+            else if(Strategy == ComboStrategy.CBS)
+            {
+                ProfitType = ProfitType.UnLimitedProfit;
+                LoseType = LoseType.LimitedLose;
+            }
+        }
+
+        public bool CalculateMaxProfit()
+        {
+            TypeJudge();
+
+            if (ProfitType == ProfitType.LimitedProfit)
+            {
+                double final_profit = 0;
+                if (Strategy == ComboStrategy.BearSpread || Strategy == ComboStrategy.Collar)
+                {
+                    MemberPos.ForEach((pos) =>
+                    {
+                        var asset = pos.Asset;
+
+                        //如果是期权头寸，则最大利润是underlying价格为0的时候
+                        if(asset.GetType()==typeof(Option))
+                        {
+                            var option = (Option)asset;
+                            double option_value = option.GetIntrinsicValue(new Price { Ask = 0, Bid = 0, Last = 0 });
+
+                            if(pos.Quantity>0)
+                            {
+                                final_profit += (option_value - pos.AvgCost) * pos.Quantity;
+                            }
+                            else
+                            {
+                                final_profit += (pos.AvgCost - option_value) * Math.Abs(pos.Quantity);
+                            }
+                        }
+                        else if(asset.GetType()==typeof(STK))
+                        {
+                            //如果是Collar的话，现货为0时候现货头寸的收益就是价格乘以数量
+                            final_profit = asset.CurrentPrice.Bid * pos.Quantity;
+                        }
+                    });
+
+                    MaxProfit = final_profit;
+
+                    return true;
+                }
+
+                return true;
+            }
+            else
+                return false;
+        }
 
         public double CalculateFPL()
         {
--- CFOTests/ComboTests.cs
+++ CFOTests/ComboTests.cs
@@ -55,5 +55,57 @@ namespace CFO.Tests
 
             Assert.AreEqual(pl, bear_spread.CalculateFPL());
         }
+
+        [TestMethod()]
+        public void CalculateMaxProfitTest()
+        {
+            Option opt1 = new Option(OptionRight.CALL, 38, "20171010");
+            Option opt2 = new Option(OptionRight.CALL, 41, "20171010");
+
+            Price opt1_price = new Price()
+            {
+                Ask = 1.85,
+                Bid = 1.84
+            };
+
+            opt1.CurrentPrice = opt1_price;
+
+            Price opt2_price = new Price()
+            {
+                Ask = 1.35,
+                Bid = 1.34
+            };
+
+            opt2.CurrentPrice = opt2_price;
+
+            Position pos1 = new Position()
+            {
+                Asset = opt1,
+                AvgCost = 3.598,
+                Quantity = -1500
+            };
+
+            Position pos2 = new Position()
+            {
+                Asset = opt2,
+                AvgCost = 2.718,
+                Quantity = 1500
+            };
+
+            Combo bear_spread = new Combo();
+            bear_spread.MemberPos.Add(pos1);
+            bear_spread.MemberPos.Add(pos2);
+
+            bear_spread.Strategy = ComboStrategy.BearSpread;
+
+            if(bear_spread.CalculateMaxProfit())
+            {
+                Assert.IsTrue(bear_spread.MaxProfit == 1320);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
     }
 }
